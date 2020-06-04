9e8acf206735244d12e286aae23ab8db04287a62
Jemima Luz <contatojemima@gmail.com>
2020-06-04 11:33:51 -0300

    suporte para desktop (responsividade)

---
+++ proj/src/views/About.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- proj/src/views/Home.vue
+++ proj/src/views/Home.vue
@@ -25,6 +25,7 @@
         div(id="bg-3-1")
         div(id="bg-3-2")
         div(id="bg-3-3")
+        div(id="bg-3-4")
 
 </template>
 
@@ -35,9 +36,9 @@ export default {
   methods: {
     parallaxScroll() {
       var scrolled = window.scrollY;
-      document.querySelector("#bg-3-1").style.top = 0 - (scrolled * 0.25) + "px";
-      document.querySelector("#bg-3-2").style.top = 0 - (scrolled * 0.3) + "px";
-      document.querySelector("#bg-3-3").style.top = 0 - (scrolled * 0.1) + "px";
+      document.querySelector("#bg-3-1").style.top = 0 - scrolled * 0.25 + "px";
+      document.querySelector("#bg-3-2").style.top = 0 - scrolled * 0.3 + "px";
+      document.querySelector("#bg-3-3").style.top = 0 - scrolled * 0.1 + "px";
     }
   },
   created() {
@@ -50,12 +51,10 @@ export default {
 @import "../styles/helpers/variables";
 
 #home {
-  background: linear-gradient(to bottom, $roxo, $preto);
   background-attachment: fixed;
 
   color: $branco;
   text-align: center;
-
 
   .logo {
     font-family: t26-carbon, monospace;
@@ -74,30 +73,22 @@ export default {
 
     p {
       background-color: $roxo-claro;
-      opacity: .5;
+      opacity: 0.5;
       border-radius: 2px;
       width: 80px;
       height: 1px;
-      margin: 20px 20px; 
+      margin: 20px 20px;
     }
   }
 
   .titulo {
     position: fixed;
-    
-    margin-top: 15vh;
 
     h2 {
       font-weight: lighter;
 
-      font-size: 28px;
-      left: 0px;
-
-      margin-bottom: 10px;
-      
       .first {
         background-color: $roxo-claro;
-        padding: 3px 0px 3px 50px;
       }
 
       .second {
@@ -105,17 +96,15 @@ export default {
         font-weight: bold;
         color: $roxo-claro;
       }
-
     }
 
-    p { 
-      font-size: 18px;
+    p {
       text-align: right;
       color: $roxo-claro;
-      
+
       span {
         color: $branco;
-        opacity: .7;
+        opacity: 0.7;
       }
     }
   }
@@ -124,20 +113,15 @@ export default {
     font-family: t26-carbon, monospace;
     position: fixed;
     display: flex;
-    width: 100vw;
     flex-direction: column;
     justify-content: center;
 
-    margin: 30vh auto 0px auto;
+    z-index: 5;
 
     div {
       display: flex;
-      margin: 0px auto;
       justify-self: center;
     }
-
-    .row-1 { margin-left: 10vw; }
-    .row-2 { margin-right: 10vw; }
 
     a {
       border: 1px solid $branco;
@@ -145,12 +129,8 @@ export default {
       color: $branco;
       z-index: 10;
 
-      transition: all .5s ease;
-      opacity: .5;
-
-      padding: 10px 20px;
-      margin: 10px;
-      font-size: 40px;
+      transition: all 0.5s ease;
+      opacity: 0.5;
 
       &:hover,
       &:focus {
@@ -161,25 +141,20 @@ export default {
 
   .about {
     position: fixed;
-    width: 100vw;
-    bottom: 100px;
-    right: -80px;
+    z-index: 5;
 
     a {
-      box-shadow: 0px 0px 10px 0px $roxo-claro;
       font-family: t26-carbon, monospace;
       border: 1px solid $branco;
       border-radius: 10px;
       color: $branco;
 
-      transition: all .5s ease;
-      opacity: .5;
-
-      font-size: 40px;
-      padding: 10px 20px;
+      transition: all 0.5s ease;
+      opacity: 0.5;
 
       &:hover,
       &:focus {
+        cursor: pointer;
         opacity: 1;
       }
     }
@@ -192,7 +167,6 @@ export default {
     div#parallax-bg-3 {
       position: fixed;
       width: 960px;
-      top: 20px;
       left: 50%;
       margin-left: -470px;
     }
@@ -203,31 +177,295 @@ export default {
       display: block;
       overflow: hidden;
     }
-
     div#bg-3-1 {
-      background: url("../assets/tcm2.svg");
-      width: 529px;
-      height: 900px;
-      right: 160px;
-      z-index: 3;
-      opacity: .8;
-    }
-    div#bg-3-2 {
-      background-image: url("../assets/tem.svg");
-      width: 529px;
-      height: 900px;
-      right: 160px;
-      z-index: 2;
-    }
-    div#bg-3-3 {
-      background: url("../assets/em.svg");
-      width: 446px;
-      height: 713px;
-      right: 180px;
-      z-index: 4;
-      opacity: .5;
+        z-index: 3;
+        opacity: 0.8;
+      }
+      div#bg-3-2 {
+        z-index: 2;
+      }
+      div#bg-3-3 {
+        z-index: 4;
+        opacity: 0.5;
+      }
+  }
+}
+
+// MOBILE
+@media only screen and (max-width: 900px) {
+  #home {
+    background: linear-gradient(to bottom, $roxo, $preto);
+
+    .titulo {
+      margin-top: 15vh;
+
+      h2 {
+        font-size: 28px;
+        left: 0px;
+
+        margin-bottom: 10px;
+
+        .first {
+          padding: 3px 0px 3px 50px;
+        }
+      }
+
+      p {
+        font-size: 18px;
+      }
     }
 
+    .projs-navgation {
+      width: 100vw;
+
+      margin: 30vh auto 0px auto;
+
+      div {
+        margin: 0px auto;
+      }
+
+      .row-1 {
+        margin-left: 15vw;
+      }
+      .row-2 {
+        margin-right: 15vw;
+      }
+
+      a {
+        padding: 10px 20px;
+        margin: 10px;
+        font-size: 30px;
+      }
+    }
+
+    .about {
+      width: 100vw;
+      bottom: 100px;
+      right: -80px;
+
+      a {
+        box-shadow: 0px 0px 10px 0px $roxo-claro;
+
+        font-size: 40px;
+        padding: 10px 20px;
+      }
+    }
+
+    .parallax {
+      /* background layer */
+      div#parallax-bg-3 {
+        top: 100px;
+      }
+
+      div#bg-3-1 {
+        background: url("../assets/tcm3.svg");
+        width: 529px;
+        height: 900px;
+        right: 160px;
+      }
+      div#bg-3-2 {
+        background-image: url("../assets/tem.svg");
+        width: 529px;
+        height: 900px;
+        right: 160px;
+      }
+      div#bg-3-3 {
+        background: url("../assets/em.svg");
+        width: 446px;
+        height: 713px;
+        right: 180px;
+      }
+      div#bg-3-4 {
+        display: none;
+      }
+    }
+  }
+}
+
+// DESKTOP
+@media only screen and (min-width: 901px) and (max-width: 2499px) {
+  #home {
+    background: linear-gradient(to bottom, $preto, $roxo);   
+
+    .titulo {
+      margin-left: 40vw;
+      margin-top: 145px;
+
+      h2 {
+        font-size: 25px;
+        left: 0px;
+
+        margin-bottom: 10px;
+
+        .first {
+          padding: 3px 0px 3px 70px;
+        }
+      }
+
+      p {
+        font-size: 18px;
+      }
+    }
+
+    .projs-navgation {
+      width: 100vw;
+
+      margin: 270px auto 0px auto;
+      
+
+      div {
+        margin: 0px auto;
+      }
+
+      .row-1 {
+        margin-left: 40vw;
+      }
+      .row-2 {
+        margin-right: 40vw;
+      }
+
+      a {
+        padding: 10px 20px;
+        margin: 10px;
+        font-size: 30px;
+      }
+    }
+
+    .about {
+      width: 100vw;
+      top: 680px;
+
+      a {
+        box-shadow: 0px 0px 30px 10px #00000030;
+
+        font-size: 25px;
+        padding: 10px 20px
+      }
+    }
+
+    .parallax {
+      div#bg-3-1 {
+        display: none;
+        background: url("../assets/d-tcm.svg");
+        width: 529px;
+        height: 800px;
+        right: 160px;
+      }
+      div#bg-3-2 {
+        background-image: url("../assets/d-tem.svg");
+        width: 529px;
+        height: 800px;
+        right: 160px;
+      }
+      div#bg-3-3 {
+        background: url("../assets/d-em.svg");
+        width: 100vw;
+        height: 100vh;
+        left: -250px;
+      }
+      div#bg-3-4 {
+        position: fixed !important;
+        background: url("../assets/d-b.svg");
+        width: 100vw;
+        height: 100vh;
+        left: 0px;
+        z-index: 4;
+        opacity: .8;
+      }
+    }
+  }
+}
+
+// WIDE
+@media only screen and (min-width: 2500px) {
+  #home {
+    background: linear-gradient(to bottom, $preto, $roxo);
+
+    .titulo {
+      margin-left: 45vw;
+      margin-top: 145px;
+
+      h2 {
+        font-size: 25px;
+        left: 0px;
+
+        margin-bottom: 10px;
+
+        .first {
+          padding: 3px 0px 3px 70px;
+        }
+      }
+
+      p {
+        font-size: 18px;
+      }
+    }
+
+    .projs-navgation {
+      width: 100vw;
+
+      margin: 270px auto 0px auto;
+
+      div {
+        margin: 0px auto;
+      }
+
+      .row-1 {
+        margin-left: 44vw;
+      }
+      .row-2 {
+        margin-right: 44vw;
+      }
+
+      a {
+        padding: 10px 20px;
+        margin: 10px;
+        font-size: 30px;
+      }
+    }
+
+    .about {
+      width: 100vw;
+      top: 680px;
+
+      a {
+        box-shadow: 0px 0px 30px 10px #00000030;
+
+        font-size: 30px;
+        padding: 10px 20px;
+      }
+    }
+
+    .parallax {
+      div#bg-3-1 {
+        display: none;
+        background: url("../assets/d-tcm.svg");
+        width: 529px;
+        height: 800px;
+        right: 160px;
+      }
+      div#bg-3-2 {
+        background-image: url("../assets/d-tem.svg");
+        width: 529px;
+        height: 800px;
+        right: 160px;
+      }
+      div#bg-3-3 {
+        background: url("../assets/d-em.svg");
+        width: 100vw;
+        height: 100vh;
+        left: -250px;
+      }
+      div#bg-3-4 {
+        position: fixed !important;
+        background: url("../assets/d-b.svg");
+        width: 100vw;
+        height: 100vh;
+        left: 0px;
+        z-index: 4;
+        opacity: .8;
+      }
+    }
   }
 }
 </style>
